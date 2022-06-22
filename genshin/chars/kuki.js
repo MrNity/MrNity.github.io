f4381dbe27f7aadd90d599046358a02952c057e1
MrNity <nitayl@list.ru>
2022-06-22 07:04:53 +0300

    Create kuki.js

---
+++ genshin/chars/kuki.js
@@ -0,0 +1,99 @@
+const Kuki = {
+    base: 0.75,
+    low: 0.15,
+    E: {
+        lvl: [
+            // lvl1
+            {
+                heal: {
+                    base: 0.696,
+                    flat: 451
+                },
+            },
+            // lvl2
+            {
+                heal: {
+                    base: 0.7482,
+                    flat: 496
+                },
+            },
+            // lvl3
+            {
+                heal: {
+                    base: 0.8004,
+                    flat: 544
+                },
+            },
+            // lvl4
+            {
+                heal: {
+                    base: 0.87,
+                    flat: 597
+                },
+            },
+            // lvl5
+            {
+                heal: {
+                    base: 0.9222,
+                    flat: 653
+                },
+            },
+            // lvl6
+            {
+                heal: {
+                    base: 0.9744,
+                    flat: 713
+                },
+            },
+            // lvl7
+            {
+                heal: {
+                    base: 1.044,
+                    flat: 777
+                },
+            },
+            // lvl8
+            {
+                heal: {
+                    base: 1.1136,
+                    flat: 845
+                },
+            },
+            // lvl9
+            {
+                heal: {
+                    base: 1.1832,
+                    flat: 916
+                },
+            },
+            // lvl10
+            {
+                heal: {
+                    base: 1.2528,
+                    flat: 991
+                },
+            },
+            // lvl11
+            {
+                heal: {
+                    base: 1.3224,
+                    flat: 1070
+                },
+            },
+            // lvl12
+            {
+                heal: {
+                    base: 1.3924,
+                    flat: 1153
+                },
+            },
+            // lvl13
+            {
+                heal: {
+                    base: 1.479,
+                    flat: 1239
+                },
+            },
+        ],
+    }
+}
